ae1952ce793075a7a782906f3ffabe9100d421ae
matheus <matheusgurther@outlook.com>
2026-05-09 02:18:49 -0300

    Criando classe entregador carro

--- src/EntregadorCarro.java
+++ src/EntregadorCarro.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class EntregadorCarro extends Entregador{
+    public EntregadorCarro(String nome){
+        getNome();
+    }
 
-public class EntregadorCarro {
+    @Override
+    void descricao() {
+        System.out.println("O pedido será entregue de carro por: " + getNome());
+    }
 }
