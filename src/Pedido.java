448539492ed6bd2771a40cf7ead2a5a698735514
matheus <matheusgurther@outlook.com>
2026-05-09 03:52:22 -0300

    Criando a classe pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class Pedido extends Entregador{
+    String cliente;
+    String produto;
+    double quantidade;
+    double valorProduto;
+    String statusPedido;
+    double valorTotal;
 
-public class Pedido {
+    public void quantidadeDeProdutos() {
+        if (quantidade <= getQuantidadeProdutos()) {
+            super.quantidadeDeProdutos();
+            System.out.println("A quantidade de " + produto + "que voce esta pedindo é " + quantidade);
+        } else {
+            throw new IllegalArgumentException("A quantidade de produtos não pode ultrapassar " + getQuantidadeProdutos());
+        }
+    }
+
+    @Override
+    void descricao() {
+        valorTotal = valorProduto + getTaxa();
+        System.out.println("Seu pedido " + cliente + "será realizado por" + getNome() + ", o seu produto está em fase de " + statusPedido + "\n o valor total junto com a taxa de entrega ficou no valor de: " + valorTotal);
+    }
 }
